266444ed4ec150a59fa9050488c059c0f88b6eb1
Dwayn Matthies <dwayn.matthies@gmail.com>
2015-03-06 22:53:34 -0800

    small tyop fix

--- src/snowflake.h
+++ src/snowflake.h
@@ -24,7 +24,7 @@ THE SOFTWARE.
 #define __SNOWFLAKE__
 
 // the timestamp in milliseconds of the start of the custom epoch
-#define SNOWFLAKE_EPOCH 1388534400000 //Midnight Januarly 1, 2014
+#define SNOWFLAKE_EPOCH 1388534400000 //Midnight January 1, 2014
 
 #define SNOWFLAKE_TIME_BITS 41
 #define SNOWFLAKE_REGIONID_BITS 4
